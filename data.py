155fb888da1048a9741afba9bffca036b4288026
Lukas Hamberg <barbabase@Lukass-Air.lan>
2018-05-02 20:26:38 +0200

    changes to data

--- data.py
+++ data.py
@@ -1,4 +1,22 @@
 class Data(object):
     #init with auto/manual
-    __init__(self,object):
+    __init__(self):
+        #init with auto/manual 0/1
+        self.mode = -1
+        self.option = -1
+
+    def set_mode(mode):
+        self.mode = mode
+        if self.option == -1:
+            self.option = 0
+
+    def set_option(option):
+        self.option = option
+        if self.mode == -1:
+            self.option = 1
+
+    def data_print():
+        print("Data: ").format(self.mode,self.option)
         
+    def data_get():
+        return {self.mode,self.option}
