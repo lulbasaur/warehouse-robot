820311982795bebe9af9133557f6aeff17b31ad4
Lukas Hamberg <barbabase@Lukass-Air.lan>
2018-05-02 20:43:24 +0200

    Changed file names, added asserts

--- ClientData.py
+++ ClientData.py
@@ -1,4 +1,6 @@
-class Data(object):
+from types import *
+
+class TCPData(object):
     #init with auto/manual
     __init__(self):
         #init with auto/manual 0/1
@@ -6,17 +8,19 @@ class Data(object):
         self.option = -1
 
     def set_mode(mode):
+        assert type(mode) is IntType, "mode is not an integer: %r" % mode
         self.mode = mode
         if self.option == -1:
             self.option = 0
 
     def set_option(option):
+        assert type(option) is IntType, "option is not an integer: %r" % option
         self.option = option
         if self.mode == -1:
             self.option = 1
 
     def data_print():
         print("Data: ").format(self.mode,self.option)
-        
+
     def data_get():
         return {self.mode,self.option}
