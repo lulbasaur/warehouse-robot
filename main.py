70a2cb4eae45dfd8cd71db24720bdb10b74886f8
Lukas Hamberg <barbabase@student243-172.eduroam.uu.se>
2018-05-04 17:05:39 +0200

    added logging for debug

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import socket
 import threading
 import TCPData
+import sys
+import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+
 
 class ThreadedServer(object):
     def __init__(self, host, port):
@@ -11,42 +17,64 @@ class ThreadedServer(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
+        print('socket binded to port', port)
+        #thread array
         self.threads = []
+
 
     def listen_for_incoming_connections(self):
         self.sock.listen(3)
+        thread_id = 1
         while True:
+            print("socket is listening")
             client, address = self.sock.accept()
+            print('Connected to :', address[0], ':', address[1])
             #time out if client is inactive
-            client.settimeout(60)
+            client.settimeout(30)
             #spawn thread for each client
-            threading.Thread(target = self.listen_to_client,args = (client,address)).start()
+            t=threading.Thread(name=str(thread_id), target=self.listen_to_client, args=(client,address))
+            self.threads.append(t)
+            thread_id = thread_id+1
+            t.start()
 
     def listen_to_client(self, client, address):
         #recieve buffer
         rx_data_size = 1024
         while True:
             try:
-                data = client.recv(rx_data_size)
-                if data:
-                    print("Data recieved")
-                    response = data.decode('utf-8')
-                    print(response)
+                data_rx = client.recv(rx_data_size)
+                if data_rx:
+                    logging.debug('Data recieved')
+                    #decode data - json probably
+                    response = data_rx.decode('utf-8')
+                    print(response)#debug, todo: remove
+                    #send echo
                     client.sendall(response.encode('utf-8'))
                 else:
-                    raise error('Client disconnected')
-            except:
+                    raise ConnectionError('Client disconnected')
+            except ConnectionError:
+                logging.debug('Connection error wih client: %s', client)
+                client.close()
+                return False
+            except socket.timeout:
+                logging.debug('Socket timeout with client: %s', client)
                 client.close()
                 return False
 
+    def processdata(self, data):
+
+
+        return data;
+
 if __name__ == "__main__":
     while True:
-        print("Initiating Warehouse Server...")
-        port_num = input("Enter port nr: ")
+        print('Initiating Warehouse Server...')
         try:
-            port_num = int(port_num)
+            server_address = input('Enter server address: ')
+            server_port = input('Enter port nr: ')
+            server_port = int(server_port)
             break
-        except ValueError:
+        except ValueError('Value error: please reenter values'):
             pass
 
-    ThreadedServer('',port_num).listen_for_incoming_connections()
+    ThreadedServer(server_address, server_port).listen_for_incoming_connections()
