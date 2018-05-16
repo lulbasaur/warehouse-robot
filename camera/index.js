9689b90dfc4b9dae121cf4eeefd021d0c8a514d3
Casper Strömberg <casper@barkh.se>
2018-05-16 11:20:40 +0200

    move to modern

--- camera/index.js
+++ camera/index.js
@@ -1,22 +1,14 @@
 const WebSocket = require("ws");
 const util = require("util");
-var cv = require("opencv");
+const cv = require("opencv");
 
-// camera properties
-var width = 620;
-var height = 440;
-var fps = 10;
+const width = 620;
+const height = 440;
+const fps = 10;
 
-// face detection properties
-var rectColor = [0, 255, 0];
-var rectThickness = 2;
-
-// initialize camera
-var camera = new cv.VideoCapture(0);
+const camera = new cv.VideoCapture(0);
 camera.setWidth(width);
 camera.setHeight(height);
-
-camera.read(console.log);
 
 const wss = new WebSocket.Server({ port: 8080 });
 
@@ -32,7 +24,7 @@ const sendFrame = () =>
   camera.read((err, image) => {
     image.detectObject(cv.FACE_CASCADE, {}, (err, faces) => {
       for (var i = 0; i < faces.length; i++) {
-        var x = faces[i];
+        let x = faces[i];
         image.ellipse(
           x.x + x.width / 2,
           x.y + x.height / 2,
