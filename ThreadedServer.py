0049e1eb998cadaa1d6b85cd8d387368015d5a24
lulbasaur <lukas.hamberg@gmail.com>
2018-05-22 16:22:07 +0200

    fix threading issue

--- ThreadedServer.py
+++ ThreadedServer.py
@@ -94,6 +94,7 @@ def listen_for_incoming_connections(sock, robots):
         t = ClientThread(t_name, client, address, robots)
         threads.append(t)
         thread_id = thread_id+1
+        t.daemon = True
         t.start()
 
 def main():
